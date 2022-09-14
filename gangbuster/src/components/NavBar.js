242b51bd96d80202efb923644f365b89ff33d4bf
ricardomejia2020 <ricardo.mejiasaldierna.704@my.csun.edu>
2022-09-13 20:27:56 -0700

    Testing

--- gangbuster/src/components/NavBar.js
+++ gangbuster/src/components/NavBar.js
@@ -30,18 +30,18 @@ const [extendNavbar, setextendNavbar] =useState(false);
                <OpenLinks>
                    <Hambuger toggled={extendNavbar} toggle={setextendNavbar} size={30}/>
                         </OpenLinks>
-                   <NavbarScroll to='Top' smooth={true} duration={200}> Home</NavbarScroll>
-                   <NavbarScroll to='About' smooth={true} duration={200} offset={-80}> About Us</NavbarScroll>
-                   <NavbarScroll to='RecentPups' smooth={true} duration={200} offset={-80}> Recent Posts</NavbarScroll>
-                   <NavbarScroll to='ContactForm' smooth={true} duration={200} offset={-80}> Contact</NavbarScroll>
+                   <NavbarScroll to='Top' smooth={true} duration={0}> Home</NavbarScroll>
+                   <NavbarScroll to='About' smooth={true} duration={0} offset={-80}> About Us</NavbarScroll>
+                   <NavbarScroll to='RecentPups' smooth={true} duration={0} offset={-80}> Recent Posts</NavbarScroll>
+                   <NavbarScroll to='ContactForm' smooth={true} duration={0} offset={-80}> Contact</NavbarScroll>
                </NavBarLinkContainer>
                </RightContainer>
            </NavbarInnerContainer>
            <NavbarExtendedContainer className={extendNavbar ? 'showContent content' : 'content'}>
-                   <ExtendedNavbarScroll to='Top'> Home</ExtendedNavbarScroll>
-                   <ExtendedNavbarScroll to='About' offset={-80}> About Us</ExtendedNavbarScroll>
-                   <ExtendedNavbarScroll to='RecentPups'offset={-80}> Recent Posts</ExtendedNavbarScroll>
-                   <ExtendedNavbarScroll to='ContactForm' offset={-80}> Contact</ExtendedNavbarScroll>
+                   <ExtendedNavbarScroll to='Top' smooth={true} duration={0}> Home</ExtendedNavbarScroll>
+                   <ExtendedNavbarScroll to='About' smooth={true} duration={0} offset={-80}> About Us</ExtendedNavbarScroll>
+                   <ExtendedNavbarScroll to='RecentPups' smooth={true} duration={0} offset={-80}> Recent Posts</ExtendedNavbarScroll>
+                   <ExtendedNavbarScroll to='ContactForm' smooth={true} duration={0} offset={-80}> Contact</ExtendedNavbarScroll>
            </NavbarExtendedContainer>
        </NavbarContainer>
     )
